0ec3bd82dfef7fc118c7c817bacd8ec6cf899db0
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-05-31 22:07:12 +0100

    new commit

--- my-app/app/prediction/[name]/page.tsx
+++ my-app/app/prediction/[name]/page.tsx
@@ -1,9 +1,40 @@
-interface Params {
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io?name=${name}`)
+  return res.json()
+}
+
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`)
+  return res.json()
+}
+
+const getPredictedCountry = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`)
+  return res.json()
+}
+
+interface params {
   params: { name: string }
 }
 
-const Page = ({ params }: any) => {
-  return <div>{params.name}</div>
-}
+export default async function Page(params: params) {
+  const ageData = getPredictedAge(params.params.name)
+  const CountryData = getPredictedCountry(params.params.name)
+  const genderData = getPredictedGender(params.params.name)
 
-export default Page
+  const [age, gender, country] = await Promise.all([
+    ageData,
+    genderData,
+    CountryData,
+  ])
+  return (
+    <div>
+      <div>
+        <div>Personal info</div>
+        <div>Age: {age?.age}</div>
+        <div>Gender: {gender?.gender}</div>
+        <div>Country: {country?.country[0]?.country}</div>
+      </div>
+    </div>
+  )
+}
